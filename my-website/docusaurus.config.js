b6303c5806690ccf2ec8ab1078770beccf7e4c08
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2026-02-16 13:11:51 +0000

    fix config

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://jessicachitas.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/plc-diary-docusaurus-site/',
+  baseUrl: '/plc-course-docusaurus-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jessicachitas', // Usually your GitHub org/user name.
-  projectName: 'plc-diary-docusaurus-site', // Usually your repo name.
+  projectName: 'plc-course-docusaurus-site', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
